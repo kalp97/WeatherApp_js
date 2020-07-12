6b5c665a3e7ff9795c0e10cba0c75d67f2825470
Kalp <kdd32@gmail.com>
2020-07-12 18:55:52 -0400

    Fetching the location and giving weather paramaters as response

--- weather.js
+++ weather.js
@@ -1,1 +1,36 @@
 // JavaScript source code
+window.addEventListener('load', ()=> {
+
+    var lat;
+    var long;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            
+           var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
+
+            
+            
+                fetch(api)
+                .then(response =>{
+                    return response.json();
+                
+				})     
+                
+                .then(data =>{
+                    console.log(data);
+                    locationTimezone.textContent = data.name;
+                    temperatureDegree.textContent = data.main.temp;
+                    temperatureDescription.textContent = data.weather[0].description
+				})     
+            });
+        }
+
+});
